98fdc79bb44d88e8c3aa8fa03d32b2277430ab17
JodliDev <jodlidev@gmail.com>
2025-08-12 11:30:31 +0200

    Fix return value for multiple chains on the same method

--- test/convenience.ts
+++ test/convenience.ts
@@ -23,20 +23,21 @@ export async function wait(ms: number = 100) {
 type Procedure = (...args: any[]) => any;
 interface ExtendedMock<T extends Procedure = Procedure> extends Mock<T> {
 	chain: (...args: any[]) => MockKysely;
+	___lastChain: (...args: any[]) => MockKysely
 }
 
 class MockKysely {
-	selectFrom = this.addChain("selectFrom");
-	select = this.addChain("select");
-	insertInto = this.addChain("insertInto");
-	values = this.addChain("values");
-	updateTable = this.addChain("updateTable");
-	set = this.addChain("set");
-	deleteFrom = this.addChain("deleteFrom");
-	innerJoin = this.addChain("innerJoin");
-	where = this.addChain("where");
-	orderBy = this.addChain("orderBy");
-	limit = this.addChain("limit");
+	selectFrom = this.makeChainable();
+	select = this.makeChainable();
+	insertInto = this.makeChainable();
+	values = this.makeChainable();
+	updateTable = this.makeChainable();
+	set = this.makeChainable();
+	deleteFrom = this.makeChainable();
+	innerJoin = this.makeChainable();
+	where = this.makeChainable();
+	orderBy = this.makeChainable();
+	limit = this.makeChainable();
 	fn = {
 		countAll: vi.fn(() => this.fn),
 		as: vi.fn(() => this.fn)
@@ -54,25 +55,28 @@ class MockKysely {
 		}
 	}
 	
-	private addChain(method: keyof this) {
+	private makeChainable() {
 		const fn = vi.fn(() => this) as ExtendedMock;
-		fn.chain = (...compareArgs: unknown[]) => this.chainMethod(method, ...compareArgs);
+		fn.chain = (...compareArgs: unknown[]) => this.chainMethod(fn, ...compareArgs);
 		return fn;
 	}
-	private chainMethod(method: keyof this, ...compareArgs: unknown[]) {
+	private chainMethod(fn: ExtendedMock, ...compareArgs: unknown[]) {
 		const instance = new MockKysely();
-		const fn = (this[method] as Mock);
-		const oldImplementation = fn.getMockImplementation();
-		fn.mockImplementation((...args) => {
-			oldImplementation?.(...args);
+		const oldImplementation = fn.___lastChain;
+		const newImplementation = (...args: any[]) => {
 			if(!compareArgs.length || JSON.stringify(args) == JSON.stringify(compareArgs)) {
 				return instance;
 			}
+			else if(oldImplementation) {
+				return oldImplementation(...args);
+			}
 			else {
-				console.log(`Encountered different arguments in ${method.toString()}: ${JSON.stringify(args)}. Expected: ${JSON.stringify(compareArgs)}`);
+				console.trace(`Could not find chain with arguments:\n${JSON.stringify(args)}.\nExpected (in first chain):\n${JSON.stringify(compareArgs)}`);
 				return this;
 			}
-		});
+		};
+		fn.___lastChain = newImplementation;
+		fn.mockImplementation(newImplementation);
 		return instance;
 	}
 	public mockResolvedChainValue(method: "executeTakeFirst" | "execute", value: unknown) {
